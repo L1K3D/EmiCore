7291efb42d50feb960473d283eef3bfc2ed5c86a
Heitor S. Ferreira <heitor.ferreira231203@gmail.com>
2025-08-08 20:49:33 -0300

    Feature: Adicionando novo menu de trabalho nos databases locais | 08.08.2025

--- prompt_version/python/functions_work_local_databases.py
+++ prompt_version/python/functions_work_local_databases.py
@@ -3,6 +3,7 @@ import os
 import time as tm
 
 from functions_basics import get_time
+from functions_database_creator import create_custom_table, create_table_from_console, read_csv_files, create_table_from_csv_file, read_sql_scripts, create_table_from_sql_script
 
 def select_database_to_work():
 
@@ -17,34 +18,19 @@ def select_database_to_work():
 
     while True:
         print("If you want exit from this step, press 0 \n")
+
         if 1 <= selection <= len(db_files):
             file_path_selected = os.path.join(folder, db_files[selection - 1])
             print(f"\nYou select: {file_path_selected}")
             return file_path_selected
-        if selection == "0":
+        
+        elif selection == "0":
             print("Exiting from this step...")
             tm.sleep(1)
             break
+        
         else:
             print("\nInvalid number, please, try again.")
-
-def list_tables_in_database(file_path_collected):
-
-    print("-----")
-
-    conn = duckdb.connect(f'{file_path_collected}')
-
-    tables_df = conn.sql("SHOW TABLES").df()
-    print(tables_df)
-    print("-----")
-
-def delete_selected_database(file_path_collected):
-    
-    db_file_path = file_path_collected
-
-    os.remove(db_file_path)
-
-    print(f"The database named {db_file_path} was deleted sucefully")
 
 def delete_databases():
 
@@ -80,6 +66,51 @@ def delete_databases():
             tm.sleep(1)
             break
 
+def list_tables_in_database(file_path_collected):
+
+    print("-----")
+
+    conn = duckdb.connect(f'{file_path_collected}')
+
+    tables_df = conn.sql("SHOW TABLES").df()
+    print(tables_df)
+    print("-----")
+
+def delete_selected_database(file_path_collected):
+    
+    db_file_path = file_path_collected
+
+    os.remove(db_file_path)
+
+    print(f"The database named {db_file_path} was deleted sucefully")
+
+def execute_custom_query(file_path_collected):
+
+    print("-----")
+    print("\nWrite your SQL query here, or write 'exit' to back to menu:")
+    print("")
+    conn = duckdb.connect(file_path_collected)
+    
+    while True:
+        query = input("SQL> ")
+
+        if query.strip().lower() == "exit":
+            break
+
+        print(">>>> ")
+
+        try:
+            result = conn.execute(query).fetchall()
+
+            if result:
+                for row in result:
+                    print(row)
+
+            print("")
+
+        except Exception as e:
+            print(f"Erro: {e}")
+
 def work_local_databases_menu():
 
     while True:
@@ -102,8 +133,10 @@ def work_local_databases_menu():
             while True:
                 workin_at_select_database_input = input(
                     "1 - List tables; \n"
-                    "2 - Execute fast SQL query; \n"
+                    "2 - Execute SQL query; \n"
+                    "3 - Create your own table using console interaction; \n"
                     "3 - Delete table; \n"
+                    "4 - Delete database; \n"
                     "0 - Exit \n\n"
                     "-> "
                 )
@@ -114,6 +147,16 @@ def work_local_databases_menu():
 
                 elif workin_at_select_database_input == "1":
                     list_tables_in_database(database_selected)
+
+                elif workin_at_select_database_input == "2":
+                    execute_custom_query(database_selected)
+
+                elif workin_at_select_database_input == "3":
+                    conn_obtained=duckdb.connect(database_selected)
+                    create_custom_table(conn_collected=conn_obtained)
+
+                elif workin_at_select_database_input == "4":
+                    delete_selected_database(database_selected)
 
         elif action_on_local_databases_input == "2":
             delete_databases()
